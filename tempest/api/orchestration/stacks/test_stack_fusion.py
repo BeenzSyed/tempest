739ecdcd2cd419e1fd33af6ad305ac8682885747
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2015-03-24 16:34:03 -0500

    Changed up delete test a bit

--- tempest/api/orchestration/stacks/test_stack_fusion.py
+++ tempest/api/orchestration/stacks/test_stack_fusion.py
@@ -332,9 +332,11 @@ class StacksTestJSON(base.BaseOrchestrationTest):
 
         #change something
         template = """
-            {"heat_template_version": "2013-05-23", "description": "Simple template to deploy a single compute instance", "resources": {"my_instance": {"type": "OS::Nova::Server", "properties": {"key_name": "primkey", "image": "CentOS 6.5", "flavor": "m1.small"}}}}}
+            {"heat_template_version": "2013-05-23", "description": "Simple template to deploy a single compute instance with an updated description to test", "resources": {"my_instance": {"type": "OS::Nova::Server", "properties": {"key_name": "primkey", "image": "CentOS 6.5", "flavor": "m1.small"}}}}}
             """
         resp, body = self.orchestration_client.update_stack_fusion(stack_name, region, template, parameters=parameters)
+
+        #verify changes
         self.assertEqual(resp['status'], '201', "expected response was 201 "
                                             "but actual was %s"%resp['status'])
         stack_identifier = body['stack']['id']
@@ -356,7 +358,6 @@ class StacksTestJSON(base.BaseOrchestrationTest):
 
 
     def test_delete_template_in_fusion(self):
-        #store a template
         template_id = self.config.orchestration['template']
 
         if template == None:
@@ -374,9 +375,29 @@ class StacksTestJSON(base.BaseOrchestrationTest):
             stack_name, region, template, parameters=parameters)
         self.assertEqual(resp['status'], '201', "expected response was 201 "
                                             "but actual was %s"%resp['status'])
-        #now delete the template and verify
+        #it exists, now delete
         dresp, dbody = self.delete_stack(stack_name, stack_identifier, region)
-        #verify it is gone
+
+        #now make sure it is gone
+        stack_identifier = body['stack']['id']
+        if resp['status'] == '201':
+            stack_id = body['stack']['id']
+            url = "stacks/%s/%s?with_support_info=1" % (stack_name, stack_id)
+            resp, body = self.orchestration_client.get_stack_info_for_fusion(
+                url, region)
+
+            #This shouldn't have worked this time
+
+            print "For test "
+            print "printing body %s " % body
+            print "only for test"
+            self.assertEqual(body['stack']['rackspace_template'], True,)
+            # self.assertEqual(body['stack']['application_name'],\
+            #                               ('WordPress'),
+            #                  "Expected wasWordpress but has "
+            #                  "no application name")
+            self.assertIn('template_id', body['stack'])
+
 
 
     def comp_list(self, list1, list2):
